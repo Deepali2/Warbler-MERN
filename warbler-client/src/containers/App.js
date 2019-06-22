4dd216f70767188011ba51f03e667d97ae5c5059
Deepali2 <hdgarg@gmail.com>
2019-06-21 21:25:04 -0700

    adds a Provider, a Router and a simple div that says Hello World

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import {Provider} from "react-redux";
+import {configureStore} from "../store";
+import {BrowserRouter as Router} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      Hello from App.js
-    </div>
-  );
-}
+const store = configureStore();
+
+const App = () => (
+  //The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function
+  <Provider store={store}>
+    <Router>
+      <div>Hello World!</div>
+    </Router>
+  </Provider>
+)
 
 export default App;
--- warbler-client/src/store/reducers/errors.js
+++ warbler-client/src/store/reducers/errors.js
@@ -1,9 +1,9 @@
 //a reducer that is going to display errors to the user
-import {ADD_ERR0R, REMOVE_ERROR} from "../actionTypes";
+import { ADD_ERROR, REMOVE_ERROR } from "../actionTypes";
 
 export default (state = {message: null}, action) => {
   switch (action.type) {
-    case ADD_ERR0R:
+    case ADD_ERROR:
       return {...state, message: action.error};
     case REMOVE_ERROR:
       return {...state, message: null};
