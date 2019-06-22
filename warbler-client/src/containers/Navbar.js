f871014b545935f88cf07220f113a3a98df48b01
Deepali2 <hdgarg@gmail.com>
2019-06-22 10:59:35 -0700

    styles the navbar

--- warbler-client/src/containers/Navbar.js
+++ warbler-client/src/containers/Navbar.js
@@ -1,20 +1,23 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import {connect} from "react-redux"; // since it is a container we also want to connect to the redux store
+import Logo from '../images/warbler-logo.png';
 
 class Navbar extends Component {
   render() {
     return(
       <nav className="navbar navbar-expand">
         <div className="container-fluid">
-          <Link to="/" className=""navbar-brand>
-             <img src="" alt="Warbler Home"/>
-          </Link>
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">
+               <img src={Logo} alt="Warbler Home"/>
+            </Link>
+            </div>        
+          <ul className="nav navbar-nav navbar-right">
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/signin">Log In</Link></li>
+          </ul>
         </div>
-        <ul className="nav navbar-nav navbar-right">
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/signin">Log In</Link></li>
-        </ul>
       </nav>
 
     );
