5d41e64808862a0c3168e612c9b89dcd504ae8c0
Deepali2 <hdgarg@gmail.com>
2019-06-23 09:29:21 -0700

    positions the signature div at the very bottom of the page

--- warbler-client/src/components/Homepage.js
+++ warbler-client/src/components/Homepage.js
@@ -4,8 +4,12 @@ import { Link } from "react-router-dom";
 const Homepage = () => (
   <div className="home-hero">
     <h1>What's Happening?</h1>
-    <h4>New to Warbler?</h4>
+    <h4>New to Warbler?</h4>    
     <Link to="/signup" className="btn btn-primary">Sign up here</Link>
+    <div className="signature">
+      <h6>Built using React, Redux, Node, Express, MongoDB</h6>
+      <h6>Designed and Maintained by deepaligarg1001@gmail.com</h6>
+    </div>
   </div>
 );
 
