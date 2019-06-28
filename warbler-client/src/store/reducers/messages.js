a40b100bb59e0cb386a6dff7b9bcb58e25ccbb01
Deepali2 <hdgarg@gmail.com>
2019-06-28 13:03:22 -0700

    deletes messages

--- warbler-client/src/store/reducers/messages.js
+++ warbler-client/src/store/reducers/messages.js
@@ -4,6 +4,8 @@ const message = (state = [], action) => {
   switch (action.type) {
     case LOAD_MESSAGES:
       return [...action.messages];
+    case REMOVE_MESSAGE:
+      return state.filter(message => message._id !== action._id);
     default:
       return state;
   }
